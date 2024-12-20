71c7a8a299a51aa8caaeb635c7f997f46d187ec6
LUTHVI <luthvi9hbdh2@gmail.com>
2024-12-20 13:29:38 +0700

    feat : api controller

---
+++ src/main/java/com/example/api/MySpringApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringApiApplication.class, args);
+    }
+}
